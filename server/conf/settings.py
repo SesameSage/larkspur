b89fac2fe1a8163906c4d91670c00adc45c65ca0
SesameSage <kcoharra2@gmail.com>
2025-04-24 18:01:40 -0400

    feat(CmdCast): accommodate multi-word input in cast command by splitting left and right sides with MuxCommand and the word "on"

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -23,9 +23,9 @@ If you want to share your game dir, including its settings, you can
 put secret game- or server-specific settings in secret_settings.py.
 
 """
+from evennia.settings_default import *
 
 # Use the defaults from Evennia unless explicitly overridden
-from evennia.settings_default import *
 
 ######################################################################
 # Evennia base server config
@@ -52,15 +52,6 @@ PROTOTYPE_MODULES = ["world.prototypes.prototypes", "world.prototypes.entity_pro
                      "world.prototypes.equipment.equipment_protos", "world.prototypes.spellcomp_protos"]
 FILE_HELP_ENTRY_MODULES = ["world.help_entries", "combat.help_combat", "typclasses.living.help_characters",
                            "typeclasses.inanimate.items.help_items", "typeclasses.ooc.help_ooc", "combat.help_magic",]
-
-REGIONS = {
-    "The First Region": {
-        "typeclass": "typeclasses.inanimate.locations.regions.Region",
-        "desc": "",
-        "attributes": [("minimum_rec_level", 1)],
-    }
-}
-GLOBAL_SCRIPTS = REGIONS
 
 ######################################################################
 # Settings given in secret_settings.py override those in this file.
