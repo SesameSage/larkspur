b11c1f1dd2b1edab5775d3de1c56256aaf58cac8
SesameSage <kcoharra2@gmail.com>
2025-04-26 18:27:37 -0400

    feat(ability): add self-targeted ability removing a negative effect

---
+++ combat/abilities/self_abilities.py
@@ -0,0 +1,41 @@
+from combat.abilities.abilities import Ability
+from combat.effects import SECS_PER_TURN, DurationEffect
+from typeclasses.base.objects import Object
+from typeclasses.living.living_entities import LivingEntity
+
+
+class EnergyTap(Ability):
+    key = "Energy Tap"
+
+    def at_object_creation(self):
+        super().at_object_creation()
+        self.db.desc = "Reinvigorate yourself with your opponents' stamina as you attack."
+        self.db.targeted = False
+        self.db.cost = ("mana", 12)
+        self.db.cooldown = 8 * SECS_PER_TURN
+
+    def func(self, caster: LivingEntity, target: Object = None):
+        caster.location.msg_contents(f"{caster.get_display_name(capital=True)} adjusts their stance to redirect their "
+                                     f"opponents' energy into their own attacks!")
+        attributes = [("effect_key", "Siphon Stamina"), ("duration", 5 * SECS_PER_TURN), ("positive", True)]
+        caster.add_effect(typeclass=DurationEffect, attributes=attributes)
+        return True
+
+
+class Expel(Ability):
+    def at_object_creation(self):
+        super().at_object_creation()
+        self.db.desc = "Collect and expel the negative aura in your body to remove a temporary debuff."
+        self.db.targeted = False
+        self.db.cost = ("mana", 16)
+        self.db.cooldown = 8 * SECS_PER_TURN
+
+    def func(self, caster: LivingEntity, target: Object = None):
+        caster.location.msg_contents(f"{caster.get_display_name(capital=True)} collects and expels negative aura.")
+        for script in caster.scripts.all():
+            if script.db.duration and not script.db.positive:
+                script.add_seconds(amt=script.db.duration)
+                script.check_duration()
+                return True
+        caster.location.msg(f"Couldn't find a negative effect on {target.get_display_name()}!")
+        return False
--- combat/abilities/self_spells.py
+++ combat/abilities/self_spells.py
@@ -19,24 +19,6 @@ class Consume(Spell):
         return True
 
 
-class EnergyTap(Spell):
-    key = "Energy Tap"
-
-    def at_object_creation(self):
-        super().at_object_creation()
-        self.db.desc = "Reinvigorate yourself with your opponents' stamina as you attack."
-        self.db.targeted = False
-        self.db.cost = ("mana", 12)
-        self.db.cooldown = 8 * SECS_PER_TURN
-
-    def func(self, caster: LivingEntity, target: Object = None):
-        caster.location.msg_contents(f"{caster.get_display_name(capital=True)} adjusts their stance to redirect their "
-                                     f"opponents' energy into their own attacks!")
-        attributes = [("effect_key", "Siphon Stamina"), ("duration", 5 * SECS_PER_TURN), ("positive", True)]
-        caster.add_effect(typeclass=DurationEffect, attributes=attributes)
-        return True
-
-
 class AbsorbEssence(Spell):
     key = "Absorb Essence"
 
