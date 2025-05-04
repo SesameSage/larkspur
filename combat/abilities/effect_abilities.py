30217ef1282b485c3bb88340c66c36a137f7d240
SesameSage <kcoharra2@gmail.com>
2025-05-04 13:49:10 -0400

    feat(ability): add Shield Bash

--- combat/abilities/effect_abilities.py
+++ combat/abilities/effect_abilities.py
@@ -4,9 +4,10 @@ from random import randint
 
 from combat.abilities.abilities import Ability
 from combat.combat_handler import COMBAT
-from combat.effects import KnockedDown, StatMod, TimedStatMod, DurationEffect
+from combat.effects import KnockedDown, StatMod, TimedStatMod, DurationEffect, DamageTypes
 from combat.combat_constants import SECS_PER_TURN
 from typeclasses.base.objects import Object
+from typeclasses.inanimate.items.equipment.apparel import Shield
 from typeclasses.living.living_entities import LivingEntity
 
 
@@ -40,6 +41,47 @@ class NeutralizingHum(Ability):
         return True
 
 
+class ShieldBash(Ability):
+    key = "Shield Bash"
+    desc = "Attempt to knock your opponent to the ground with your shield."
+
+    def at_object_creation(self):
+        super().at_object_creation()
+
+        self.db.targeted = True
+        self.db.must_target_entity = False
+
+        self.db.requires = [("strength", 4)]
+
+        self.db.ap_cost = 3
+        self.db.cost = [("stamina", 20)]
+        self.db.cooldown = 6 * SECS_PER_TURN
+
+    def check(self, caster, target):
+        super().check(caster, target)
+        if not isinstance(caster.db.equipment["secondary"], Shield):  # If caster doesn't have a shield equipped
+            caster.msg("You don't have a shield equipped!")
+            return False
+        return True
+
+    def func(self, caster: LivingEntity, target: Object = None):
+        caster.location.msg_contents(f"{caster.get_display_name(capital=True)} uses their shield to charge at "
+                                     f"{target.get_display_name(article=True)} with the full weight of their body!")
+
+        strength_factor = caster.get_attr("str")
+        shield_factor = caster.db.equipment["secondary"].db.defense[None]
+        knockdown_power = strength_factor + shield_factor
+
+        damage = shield_factor
+        COMBAT.resolve_attack(attacker=caster, defender=target, attack=self, damage_values={DamageTypes.BLUNT: damage})
+
+        knockdown_resistance = target.get_attr("constitution") + randint(1, 3)
+        if knockdown_resistance > knockdown_power:
+            caster.location.msg_contents(f"{target.get_display_name(capital=True)} stands strong!")
+        else:
+            target.add_effect(KnockedDown, attributes=[("source", self.key)])
+
+
 class SolarPlexusStrike(Ability):
     key = "Solar Plexus Strike"
     desc = ("Strike at the center of power in the body, weakening your opponent's attack and potentially "
@@ -70,7 +112,6 @@ class SolarPlexusStrike(Ability):
             if randint(1, 3) > 1:
                 attributes = [("effect_key", "Winded"), ("duration", 3 * SECS_PER_TURN), ("source", self.key)]
                 target.add_effect(typeclass=DurationEffect, attributes=attributes)
-
 
 
 class Sweep(Ability):
