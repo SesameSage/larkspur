40722768c932d2ee10303897f5e395ef3d7a4398
SesameSage <kcoharra2@gmail.com>
2025-05-10 00:17:00 -0400

    fix(CmdAttack): fix attribute access on tile effect check

--- commands/combat_commands.py
+++ commands/combat_commands.py
@@ -102,10 +102,10 @@ class CmdAttack(Command):
         if attacker.db.combat_ap < attacker.ap_to_attack():
             attacker.msg("Not enough AP!")
             return
-        tile_effect = self.db.combat_turnhandler.db.grid.effect_at(self.db.combat_x, self.db.combat_y)
+        tile_effect = self.caller.db.combat_turnhandler.db.grid.effect_at(self.caller.db.combat_x, self.caller.db.combat_y)
         if tile_effect:
             if tile_effect.db.effect_key == "Swarm":
-                self.db.msg("Insects swarm around your face, preventing you from attacking!")
+                self.caller.msg("Insects swarm around your face, preventing you from attacking!")
                 return
 
         attacker.attack(target)
