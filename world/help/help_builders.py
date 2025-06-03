e1311c1455a7f9661afe14fa71e2e8105c8c0162
SesameSage <kcoharra2@gmail.com>
2025-06-03 14:08:33 -0400

    docs(quests): general docs on quests

--- world/help/help_builders.py
+++ world/help/help_builders.py
@@ -1,7 +1,14 @@
-from world.quests import quest_hooks
+from world.quests import quest_hooks, quest
 
 LOCKS = "view:perm(Builder); read:perm(Builder)"
 
+HELP_QUEST_BUILD = {
+    "locks": LOCKS,
+    "key": "quest building",
+    "aliases": ["quest build", "questbuild"],
+    "category": "Building",
+    "text": quest.__doc__
+}
 HELP_QUEST_HOOKS = {
     "locks": LOCKS,
     "key": "quest hooks",
--- world/quests/quest.py
+++ world/quests/quest.py
@@ -1,6 +1,29 @@
+"""
+Quests are identified with a quest ID number (QID), and are broken up into stages identified by stage number.
+Player characters store their quest progress in a quest_stages dict matching active QIDs to the player's current stage.
+
+A global "All Quests" script contains a dictionary which stores each created quest's QID, level, descriptions, and each
+stage's data by number, including descriptions and automatically set references to the hook type, object, and location.
+Use the 'questedit' command to view this data.
+The 'desc' attribute of quests and stages are the simplest definition shown to players, e.g. quests like "Begin your
+journey" or "Help an injured hunter," and stages like "Talk to Attoah," "Pick up the quarterstaff," "Kill 5 cultists",
+etc.
+The 'long_desc' attribute is akin to the player character's journal/log entry, e.g. "I've come across a hunter on the
+road. It looks like he's hurt.", or "I found the relic in the tomb, but it's not what Lorto expected. I should return to
+talk to him."
+
+The hook type, object, and location entries are references that are set when a quest hook is created on an object.
+Quest hooks are defined and stored in the attributes of the associated object, and are solely responsible for the logic
+of quest objectives. Players advance through stages in quests by triggering particular quest hooks while they are at
+the associated quest stage. Hooks are triggered by performing actions such as talking to NPCs, defeating enemies, or
+giving and getting items.
+See 'help quest hooks' for more information.
+"""
+
 from evennia import GLOBAL_SCRIPTS
 
 from typeclasses.scripts.scripts import Script
+
 
 def all_quests():
     quests = GLOBAL_SCRIPTS.get("All Quests").db.quests
