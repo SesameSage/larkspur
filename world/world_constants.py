a321d6eeeba05e813a391bc78f70c8ded55a6efb
SesameSage <kcoharra2@gmail.com>
2026-02-21 14:22:22 -0500

    fix(characters): ensure only one default talk response is assigned to a character

--- world/world_constants.py
+++ world/world_constants.py
@@ -1,7 +1,7 @@
 INDOOR_ENVIRONMENTS = ["wood room", "stone room", "cave"]
 DEFAULT_TALK_RESPONSES = [
-    "Hi there."
-    "Hello."
-    "G'day!"
+    "Hi there.",
+    "Hello.",
+    "G'day!",
     "How're you?"
 ]
