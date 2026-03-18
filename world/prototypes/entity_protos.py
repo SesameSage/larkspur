56ebb3e07f929784cc5a425e50aa1eed458d2d04
SesameSage <kcoharra2@gmail.com>
2026-03-17 22:09:26 -0400

    fix(prototype): fix an outdated reference in entity_protos.py

--- world/prototypes/entity_protos.py
+++ world/prototypes/entity_protos.py
@@ -4,7 +4,8 @@ from combat.abilities.damage_abilities import Scratch
 
 HELLHOUND = {
     "key": "hellhound",
-    "typeclass": "typeclasses.living.creatures.EnemyBeast",
+    "typeclass": "typeclasses.living.creatures.Creature",
+    "hostile_to_players": "True",
     "char_defense": {None: 5},
     "evasion": 15,
     "abilities": [create_object(typeclass=Scratch, key="Scratch")]
