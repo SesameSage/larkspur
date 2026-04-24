cd714c8dd5149d4aaf7b8f3c54f4d6847ba0ee0f
SesameSage <kcoharra2@gmail.com>
2026-04-24 16:00:47 -0400

    feat(leveling): reduce frequency of level up reminders

--- typeclasses/scripts/player_scripts.py
+++ typeclasses/scripts/player_scripts.py
@@ -5,7 +5,7 @@ from typeclasses.scripts.scripts import Script
 class LevelUpReminder(Script):
     def at_script_creation(self):
         self.key = "LevelUpReminder"
-        self.interval = 120
+        self.interval = 600 # Remind every 10 minutes
 
     def at_repeat(self, **kwargs):
         self.obj.msg(appearance.notify + "Reminder: You have enough experience to level up!")
